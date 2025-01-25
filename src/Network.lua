2f461019377eafa29576c4fb7bcefebc7322d9c0
OneX <118699639+Local-OneX@users.noreply.github.com>
2025-01-25 16:12:22 -0500

    Update Network.lua

--- src/Network.lua
+++ src/Network.lua
@@ -1,1 +1,9 @@
+--TODO: Initilize ps99 modules to work on Solara executor, because Solara doesn't support `require`.
 
+local Network = {}
+
+local function _getRemote(remoteName)
+	local remote = game.ReplicatedStorage:WaitForChild("Network"):WaitForChild(remoteName)
+	
+	return remote
+end 
