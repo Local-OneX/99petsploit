34a84835f18e8742825db40c1879783c6c2327e3
OneX <118699639+LocalOneX@users.noreply.github.com>
2025-02-22 12:54:54 -0500

    Update zoneextractor.lua

--- src/zoneextractor.lua
+++ src/zoneextractor.lua
@@ -1,5 +1,6 @@
 ---@LocalOnex
----TODO: Extract entire map to your workspace folder.
+---Make sure to stop library from loading
+---TODO: Extract entire map to your workspace folder. 
 
 map = map or workspace:FindFirstChild("Map")
 map = map or workspace:FindFirstChild("Map2")
@@ -9,13 +10,22 @@ synsaveinstance = loadstring(game:HttpGet("https://raw.githubusercontent.com/lua
 
 player = game.Players.LocalPlayer
 character = player.Character
-rootPart = character.Humanoid:FindFirstChild("HumanoidRootPart")
-zonesFolder = Instance.new("Folder")  
-BUFFER_DELTA = .1
+rootPart = character:FindFirstChild("HumanoidRootPart")
+torso = player.Character:FindFirstChild("UpperTorso")
+zonesFolder = workspace.ALWAYS_RENDERING:Clone()
+zonesFolder.Name = map.Name
+zonesFolder:ClearAllChildren()
+BUFFER_DELTA = 1.5
+SAVE_INDIVIDUAL = false
+
+workspace.StreamingEnabled = false
+torso.Anchored = true
 
 warn("Initilizing")
+warn(string.format("This will take ≈%s seconds!", #map:GetChildren() * BUFFER_DELTA))
+extracted = 0
 for i, v in ipairs(map:GetChildren()) do
-	
+
 	local pos;
 	if v:FindFirstChild("SPAWNS") then
 		pos = v:FindFirstChild("SPAWNS"):FindFirstChild("Spawn").Position
@@ -24,27 +34,31 @@ for i, v in ipairs(map:GetChildren()) do
 	elseif v:FindFirstChild("GROUND") then
 		pos = v:FindFirstChild("GROUND"):FindFirstChild("Ground").Position
 	end
-	
+
 	if not pos then
 		warn(string.format("Couldn't save %s", v.Name))
 		continue
 	end
-	
+
+	character:PivotTo(CFrame.new(pos)) 
 	local object = v:Clone()
 	object.Parent = zonesFolder
-	
-	synsaveinstance {
-		Object = object,
-		FilePath = object.Name
-	}
-	
-	print(string.format("Successfully extracted %s", v.Name))
+
+	if SAVE_INDIVIDUAL then
+		synsaveinstance {
+			Object = object, 
+		}
+	end
+
+	extracted += 1
+
+	print(string.format("Successfully extracted %s ≈%s seconds left.", v.Name, (#map:GetChildren() - extracted) * BUFFER_DELTA))
 	task.wait(BUFFER_DELTA)
 end
 
+torso.Anchored = false 
 synsaveinstance {
-	Object = zonesFolder,
-	FilePath = map.Name.."_EXTRACTED"
+	Object = zonesFolder, 
 }
 
-print(string.format("Successfully extracted %s", "Map")) 
+print(string.format("Successfully extracted %s", map.Name)) 
